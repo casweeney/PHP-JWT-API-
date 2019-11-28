304335cb8055bc39ae7928d002e5bfa183317326
Casweeney <casweeno2000@gmail.com>
2019-11-28 23:34:52 +0100

    still working on api

--- api.php
+++ api.php
@@ -1,7 +1,25 @@
 <?php
 	class Api extends Rest {
+		public $dbConn;
 		public function __construct() {
 			parent::__construct();
+			$db = new DbConnect;
+			$this->dbConn = $db->connect();
+		}
+
+		public function generateToken() {
+			$email = $this->validateParameter('email', $this->param['email'], STRING);
+			$pass = $this->validateParameter('pass', $this->param['pass'], STRING);
+
+			$stmt = $this->dbConn->prepare("SELECT * FROM users WHERE email = :email AND password = :pass");
+			$stmt->bindParam(":email", $email);
+			$stmt->bindParam(":pass", $pass);
+			$stmt->execute();
+
+			$user = $stmt->fetch(PDO::FETCH_ASSOC);
+			if(!is_array($user)){
+				$this->returnResponse(INVALID_USER_PASS, 'Email or Passord is incorrect.');
+			}
 		}
 	}
 ?>
--- rest.php
+++ rest.php
@@ -25,15 +25,60 @@
 			}
 
 			$data = json_decode($this->request, true);
-			print_r($data);
+
+			if(!isset($data['name']) || $data['name'] == ""){
+				$this->throwError(API_NAME_REQUIRED, 'API name is required.');
+			}
+
+			$this->serviceName = $data['name'];
+
+
+			if(!is_array($data['param'])){
+				$this->throwError(API_PARAM_REQUIRED, 'API PARAM is required.');
+			}
+
+			$this->param = $data['param'];
 		}
 
 		public function processApi() {
-
+			$api = new Api;
+			$rMethod = new reflectionMethod('Api', $this->serviceName);
+			if(!method_exists($api, $this->serviceName)) {
+				$this->throwError(API_DOST_NOT_EXIST, 'API does not exist');
+			}
+			$rMethod->invoke($api);
 		}
 
-		public function validateParameter($fieldName, $value, $dataType, $required) {
+		public function validateParameter($fieldName, $value, $dataType, $required = true) {
+			if($required == true && empty($value) == true){
+				$this->throwError(VALIDATE_PARAMETER_REQUIRED, $fieldName . ' parameter is required.');
+			}
 
+			switch ($dataType) {
+				case BOOLEAN:
+					if(!is_bool($value)) {
+						$this->throwError(VALIDATE_PARAMETER_DATATYPE, 'Datatype is not valid for ' . $fieldName . '. It should be boolean');
+					}
+					break;
+
+				case INTEGER:
+					if(!is_numeric($value)) {
+						$this->throwError(VALIDATE_PARAMETER_DATATYPE, 'Datatype is not valid for ' . $fieldName . '. It should be numeric.');
+					}
+					break;
+
+				case STRING:
+					if(!is_string($value)) {
+						$this->throwError(VALIDATE_PARAMETER_DATATYPE, 'Datatype is not valid for ' . $fieldName . '. It should be string.');
+					}
+					break;
+				
+				default:
+					$this->throwError(VALIDATE_PARAMETER_DATATYPE, 'Datatype is not valid for ' . $fieldName);
+					break;
+			}
+
+			return $value;
 		}
 
 		public function throwError($code, $message) {
@@ -43,8 +88,9 @@
 			exit;
 		}
 
-		public function returnResponse() {
-
+		public function returnResponse($code, $data) {
+			header("content-type: application/json");
+			$response = json_encode(['response' => ])
 		}
 
 	}
